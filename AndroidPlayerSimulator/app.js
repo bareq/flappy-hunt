2e3d4014aa6f220065598ee90e7ac9be5dd71b0d
Paweł Auguściński <augustlolek@gmail.com>
2015-06-07 17:27:54 +0200

    Szablon strony do testowania.

---
+++ AndroidPlayerSimulator/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		title: 'PlayerSimulator'
+	});
+})
+
+app.listen(1337, function () {
+	console.log('ready on port 1337');
+})
